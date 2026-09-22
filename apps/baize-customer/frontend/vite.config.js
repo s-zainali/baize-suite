eedabc23369cc436bfd838c39cff83740f78bace
Syed Zain Ali <szainali284@gmail.com>
2026-09-23 00:07:08 +0500

    added pwa support for customer app

--- apps/baize-customer/frontend/vite.config.js
+++ apps/baize-customer/frontend/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,30 @@ export default defineConfig({
     vue(),
     tailwindcss(),
     vueDevTools(),
+    VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'auto',
+        manifest: {
+          name: "Baize",
+          short_name: 'Baize',
+          description: 'Games Social and Bookings',
+          theme_color: '#0f172a',
+          background_color: '#0f172a',
+          display: 'standalone',
+          icons: [
+            {
+              src: '/pwa-192x192.png', // Add these image files to your public/ folder
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
   ],
   server: {
     host: '0.0.0.0', // Exposes the app to the local network
